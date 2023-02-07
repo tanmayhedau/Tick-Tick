d9e299693fbce528a14ae359c5af5b085728e065
tanmayhedau <tanmayhedau619@gmail.com>
2023-02-07 11:00:46 +0530

    final push

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/api/tasks", router);
+app.get("/api/tasks", router);
 
 
 
